f58297168c9367b0d7295a4ac3c1b754c7ab4c58
Dani0619 <daniacode123@gmail.com>
2026-06-17 12:12:22 +0530

    updated

---
+++ demo.js
@@ -0,0 +1,24 @@
+/*let a = 12
+let b = 23
+function samsung()
+{
+    console.log(a+b)
+}
+function redmi()
+{
+    console.log("This is redmi")
+}
+function vivo(a,b){
+    console.log(a*b)
+}
+samsung()
+vivo(2,4)
+redmi()
+
+
+function ho(){
+    return "hi"
+} 
+let a = ho()
+console.log(a)
+*/
